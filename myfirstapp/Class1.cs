a7c4588675b0b416c5d741e8ad8dde011a13cd97
SdelgadoU <samuel2007toma@gmail.com>
2024-05-13 17:39:51 -0500

    Develop (#7)
    * Second commit
    
    * Commit andres (#1)
    
    Co-authored-by: Andres Urrego <andres_urrego@epam.com>
    
    * add sum fuction to addd class (#2)
    
    Co-authored-by: Andres Urrego <andres_urrego@epam.com>
    
    * addclassss (#3)
    
    Co-authored-by: Andres Urrego <andres_urrego@epam.com>
    
    * myfirstappadd (#4)
    
    Co-authored-by: Andres Urrego <andres_urrego@epam.com>
    
    * featuremodifymetod (#5)
    
    Co-authored-by: Andres Urrego <andres_urrego@epam.com>
    
    * Add changes (#6)
    
    Co-authored-by: Andres Urrego <andres_urrego@epam.com>
    
    ---------
    
    Co-authored-by: Andres Urrego <andres_urrego@epam.com>

---
+++ myfirstapp/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myfirstapp
+{
+    internal class Class1
+    {
+
+        public int Number { get; set; }
+        public Class1(int number)
+        {
+            Number = number;
+        }
+
+        public int GetSqurt() 
+        {
+            return Number*Number;
+        }
+        public int sum(int x)
+        { return x + x; }
+
+
+    }
+}
---
+++ myfirstapp/Class2Sam.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myfirstapp
+{
+    internal class Class2Sam
+    {
+        public int number { get; set; }
+        public Class2Sam(int number)
+        {
+
+            this.number = number;
+
+        }
+        public int minusnum(int a,  int b,int c)
+        { 
+        return a - b - c * number;
+
+        }
+    }
+}
